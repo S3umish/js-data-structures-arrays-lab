e7a395ba850e1c329ac76d3f3a1861b7133340ad
sunitee <suniteebajpai2@gmail.com>
2021-03-06 15:17:57 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+    return drivers.push('Ralph');
+}
+
+
+
+function destructivelyPrependDriver(name) {
+    debugger
+    return drivers.unshift("Bob");
+}
+
+function destructivelyRemoveLastDriver() {
+    return drivers.pop(-1);
+}
+
+function destructivelyRemoveFirstDriver() {
+    return drivers.shift(drivers.first);
+}
+
+function appendDriver(name) {
+    return [...drivers,name];
+}
+
+function  prependDriver(name) {
+    return [name, ...drivers];
+}
+
+function  removeFirstDriver() {
+    return drivers.slice(1);
+}
+
+
+function removeLastDriver () {
+    return drivers.slice(0, drivers.length - 1);
+  }
+ 
+
+
+
+
+
